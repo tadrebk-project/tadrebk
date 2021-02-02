617b7b65b51a54c6e4166623adcdd42e0be515b1
sasmaq <37103211+sasmaq@users.noreply.github.com>
2021-02-02 11:36:21 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="project.css">
+
+    <title>Login</title>
+</head>
+
+<body>
+
+
+    <!-- Just an image -->
+    <nav class="navbar navbar-light" style="background-color: #ffffff;">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="resources/Tadreebk.png" alt="" width="175" height="44">
+            </a>
+        </div>
+    </nav>
+
+    Hello, <?php echo $_SESSION['type'] ?>.
+
+</body>
+
+</html>
