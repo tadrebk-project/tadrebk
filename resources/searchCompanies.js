bb5252b5e3f5a26a2381b8bf3b78308171f37063
mohesk42 <62697562+mohesk42@users.noreply.github.com>
2021-02-16 20:06:08 +0300

    Add files via upload

--- resources/searchCompanies.js
+++ resources/searchCompanies.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     
 	function load_data(query)
 	{
-        
 		$.ajax({
 			url:"getData/searchCompanies.php",
 			method:"post",
@@ -14,8 +13,20 @@ $(document).ready(function(){
 		});
 	}
 
+	function filter(majorID,location,trainingType)
+	{
+		$.ajax({
+			url:"getData/filterCompanies.php",
+			method:"post",
+			data:{majorID:majorID,location:location,trainingType:trainingType},
+			success:function(data)
+			{
+				$('#companiesList').html(data);
+			}
+		});
+	}
+
 	$('#search').keyup(function(){
-        
 		var search = $(this).val();
 		if(search != '')
 		{
@@ -25,5 +36,24 @@ $(document).ready(function(){
 		{
 			load_data();
 		}
+	});
+
+	$('#majorSelect').change(function(){
+		var majorID = $('#majorSelect').val();
+		var location = $('#locationSelect').val();
+		var trainingType = $('#trainingTypeSelect').val();
+		filter(majorID,location,trainingType);
+	});
+	$('#locationSelect').change(function(){
+		var majorID = $('#majorSelect').val();
+		var location = $('#locationSelect').val();
+		var trainingType = $('#trainingTypeSelect').val();
+		filter(majorID,location,trainingType);
+	});
+	$('#trainingTypeSelect').change(function(){
+		var majorID = $('#majorSelect').val();
+		var location = $('#locationSelect').val();
+		var trainingType = $('#trainingTypeSelect').val();
+		filter(majorID,location,trainingType);
 	});
 });
