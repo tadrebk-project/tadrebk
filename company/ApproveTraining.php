44d718149244e69d407cebbf5ca66307e34a6429
mohesk42 <62697562+mohesk42@users.noreply.github.com>
2021-02-28 19:26:53 +0300

    Update ApproveTraining.php

--- company/ApproveTraining.php
+++ company/ApproveTraining.php
@@ -23,7 +23,7 @@ else{
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="company.css">
     <script src="resources/filterStudents.js"></script>
 
     <link rel="icon" type="image/png" href="../general_resources/Tadreabk favicon.png" />
