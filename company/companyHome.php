8fb496d4c48503fb219c011560650aa93d10de13
Mohammad Owaidah <eng.mmo7@gmail.com>
2021-03-01 20:13:24 +0300

    1

--- company/companyHome.php
+++ company/companyHome.php
@@ -25,7 +25,7 @@ else{
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    
+
     <link rel="stylesheet" href="company.css">
 
     <link rel="icon" type="image/png" href="../general_resources/Tadreabk favicon.png" />
@@ -79,7 +79,7 @@ else{
                 <a href="trainingApplications.php" class="text-decoration-none">
                     <div class="card" style="height: 20rem;">
                         <div class="card-body d-flex align-items-end">
-                            <p class="text mb-0">Application</p>
+                            <p class="text mb-0">Applications</p>
                         </div>
                     </div>
                 </a>
