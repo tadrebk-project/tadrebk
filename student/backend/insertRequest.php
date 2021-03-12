e4dee87d36027a524bed48fad9108d0d7ca120fe
Mohammad Muneer <mohammadesk42@gmail.com>
2021-03-12 23:17:37 +0300

    Update insertRequest.php
    Added some errors checking for request applications

--- student/backend/insertRequest.php
+++ student/backend/insertRequest.php
@@ -14,40 +14,62 @@ else{
 }
 
 if (isset($_POST['add_request'])) {
-    $query2 = "SELECT * FROM Student where studentID = ".$_SESSION['studentid'];
+    $query2 = "SELECT * FROM student where studentID = ".$_SESSION['studentid'];
     $HasCompany = true;
-if($result2=mysqli_query($conn, $query2)){
-    if(mysqli_num_rows($result2)>0){
-        //(here table)
-        $str = "";
-        while($row2=mysqli_fetch_array($result2)){
-            if($row2["compID"] ==""){
-                $HasCompany = false;
+    $HasCV = true;
+    $alreadyRequested = true;
+    if($result2=mysqli_query($conn, $query2)){
+        if(mysqli_num_rows($result2)>0){
+            while($row2=mysqli_fetch_array($result2)){
+                if($row2["compID"] ==""){
+                    $HasCompany = false;
+                }
+                else{
+                    $HasCompany = true;
+                }
+                if($row2["CVFileRef"] ==""){
+                    $HasCV = false;
+                }
+                else{
+                    $HasCV = true;
+                }
             }
-            else{
-                $HasCompany = true;
-            }
-             
         }
-        
-        
+        else {
+            echo "no id";
+        }
     }
-    else {
-        echo "no id";
+    $query3 = "SELECT * FROM studentrequest where appID = '".$_GET['appID']."' and studentID = ".$_SESSION['studentid'];
+    if($result3=mysqli_query($conn, $query3)){
+        if(mysqli_num_rows($result3)>0){
+            $alreadyRequested = true;
+        }
+        else {
+            $alreadyRequested = false;
+        }
     }
-}
-    if(!$HasCompany){
+    if(!$HasCompany&&$HasCV&&!$alreadyRequested){
         $appID = $_GET['appID'];
         $studentID = $_SESSION['studentid'];
         $date = date("Y-m-d");
         $status = 'Pending';
-        $query = "INSERT INTO StudentRequest (appID, studentID, date, status) VALUES('$appID','$studentID','$date','$status');";
+        $query = "INSERT INTO studentrequest (appID, studentID, date, status) VALUES('$appID','$studentID','$date','$status');";
         mysqli_query($conn, $query);
         mysqli_close($conn);
         header('location: ../applications.php?compID='.$_GET['compID']);
     }
-    else {
+    elseif($HasCompany) {
         echo "<script>alert('You are already associated with a company!');
+        window.location.href='../studentHome.php';</script>";
+        //header('location: ../studentHome.php');
+    }
+    elseif(!$HasCV) {
+        echo "<script>alert('Please upload your CV through your profile.');
+        window.location.href='../studentHome.php';</script>";
+        //header('location: ../studentHome.php');
+    }
+    elseif($alreadyRequested) {
+        echo "<script>alert('You have already requested this application!');
         window.location.href='../studentHome.php';</script>";
         //header('location: ../studentHome.php');
     }
