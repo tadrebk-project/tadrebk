00d19ce5741b4b43f099eca35a2006a049445320
Mohammad Muneer <mohammadesk42@gmail.com>
2021-03-28 14:39:20 +0300

    Added error check to register students

--- admin/backend/registerStudents.php
+++ admin/backend/registerStudents.php
@@ -15,7 +15,7 @@ else{
 
 if (isset($_POST['registerByFile'])) {
     $majorIndex["temp"] = "";
-    $query = "SELECT * FROM Major;";
+    $query = "SELECT * FROM major;";
     if($result=mysqli_query($conn, $query)){
         if(mysqli_num_rows($result)>0){
             while($row=mysqli_fetch_array($result)){
@@ -24,18 +24,30 @@ if (isset($_POST['registerByFile'])) {
         }
     }
 
+    //Get all registered students IDs
+    $query = "SELECT studentID FROM student;";
+    if($result=mysqli_query($conn, $query)){
+        if(mysqli_num_rows($result)>0){
+            $i=0;
+            while($row=mysqli_fetch_array($result)){
+                $studentIDList[$i] = $row['studentID'];
+                $i++;
+            }
+        }
+    }
+
+    $numOfStudentsInFile = 0;
+    $numOfStudentsAdded = 0;
 
     $fileName = $_FILES["inputCSV"]["tmp_name"];
     //Data in file format: studentID, name, gpa, MID
-    if ($_FILES["inputCSV"]["size"] > 0) {
-        
+    if ($_FILES["inputCSV"]["size"] > 0) {   
         $file = fopen($fileName, "r");
-        
         while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
-            
+            $numOfStudentsInFile++;
             $studentID = "";
             if (isset($column[0])) {
-                $studentID = mysqli_real_escape_string($conn, $column[0]);
+                $studentID = mysqli_real_escape_string($conn, trim($column[0]));
             }
             $name = "";
             if (isset($column[1])) {
@@ -52,15 +64,20 @@ if (isset($_POST['registerByFile'])) {
             
             $username = "s".$studentID;
             $password = "Stu#".$studentID;
-            $query = "INSERT INTO user1 (username, password, type) VALUES('$username','$password','student');";
-            mysqli_query($conn, $query);
-            $last_userID = mysqli_insert_id($conn);
+            if(!in_array($studentID,$studentIDList)){
+                $query = "INSERT INTO user1 (username, password, type) VALUES('$username','$password','student');";
+                mysqli_query($conn, $query);
+                $last_userID = mysqli_insert_id($conn);
 
-            $query = "INSERT INTO student (userID, studentID, name, gpa, MID) VALUES('$last_userID','$studentID','$name','$gpa','$MID');";
-            mysqli_query($conn, $query);     
+                $query = "INSERT INTO student (userID, studentID, name, gpa, MID) VALUES('$last_userID','$studentID','$name','$gpa','$MID');";
+                mysqli_query($conn, $query);
+                $numOfStudentsAdded++;
+            }     
         }
     }
-    header('location: ../manageStudents.php');
+    $msg= $numOfStudentsAdded." out of ".$numOfStudentsInFile." students have been registered.";
+    echo "<script type='text/javascript'>alert('$msg');</script>;
+        <script type='text/javascript'>window.location.href = '../manageStudents.php';</script>";
 }
 elseif (isset($_POST['registerManually'])) {
     $studentID = mysqli_real_escape_string($conn, $_POST['inputStudentID']);
