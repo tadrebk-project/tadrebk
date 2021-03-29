639d02c477b81fadfc2374adabe389d7d1cd9e3a
sasmaq <37103211+sasmaq@users.noreply.github.com>
2021-03-29 14:51:11 +0300

    added index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="student/project.css">
+
+    <link rel="icon" type="image/png" href="general_resources/Tadreabk favicon.png" />
+    <title>Welcome</title>
+</head>
+
+<body>
+
+
+    <!-- Just an image -->
+    <nav class="navbar navbar-light" style="background-color: #ffffff;">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="general_resources/Tadreabk logo.png" alt="" width="270" height="50">
+            </a>
+        </div>
+    </nav>
+
+    <div class="d-flex justify-content-center Center-Container">
+        <div class="card align-self-center mx-3" style="width: 30rem;">
+            <div class="card-body">
+                <div class="container text-center">
+                    <span class="text">Welcome!</span>
+                    <br><br>
+                    <a class="btn btn-primary" href="Login.html" role="button">Login</a>
+                </div>
+            </div>
+        </div>
+
+</body>
+
+</html>
