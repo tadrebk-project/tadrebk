ccfa24ace8abdfa5128db052dcad72babd43d591
hellRagoon <78369258+hellRagoon@users.noreply.github.com>
2021-03-31 03:53:55 +0300

    Update manageInstructors.php

--- admin/manageInstructors.php
+++ admin/manageInstructors.php
@@ -33,7 +33,7 @@ if (file_exists("../general_backend/sessionStart.php")) {
     <link rel="stylesheet" href="admin.css">
 
     <link rel="icon" type="image/png" href="../general_resources/Tadreabk favicon.png" />
-    <title>Manage Instructor</title>
+    <title>Manage Instructors</title>
 </head>
 
 <body>
@@ -62,106 +62,33 @@ if (file_exists("../general_backend/sessionStart.php")) {
 
     <div class="container mx-auto my-4">
         <div class="card">
-            <div class="card-body px-0">
-                <nav>
-                    <div class="nav nav-tabs px-3" id="nav-tab" role="tablist">
-                        <div class="d-flex align-self-center pe-3"></div>
-                        <button class="nav-link active tap-btn" id="nav-register-tab" data-bs-toggle="tab" data-bs-target="#nav-register" type="button" role="tab" aria-controls="nav-register" aria-selected="true">Register Instructor</button>
-                        <button class="nav-link tap-btn" id="nav-csv-tab" data-bs-toggle="tab" data-bs-target="#nav-csv" type="button" role="tab" aria-controls="nav-csv" aria-selected="false">Delete Instructor</button>
+            <div class="card-body">
+                <div class="mx-3">
+                    <div class="d-md-flex justify-content-between">
+                        <p class="text mb-0">Manage Instructor</p>
+                        <a href="addInstructor.php" class="btn btn-primary my-1">Add Instructor</a>
                     </div>
-                </nav>
-                <div class="tab-content" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="nav-register" role="tabpanel" aria-labelledby="nav-register-tab">
-                        <div class="px-3 pt-3">
-                            <form action="backend/registerStudents.php" method="post">
-                                <div class="mb-3 row">
-                                    <label for="inputInstructorName" class="col-sm-2 col-form-label">Name</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="inputInstructorName" name="inputInstructorName" required>
-                                    </div>
-                                </div>
-                                <div class="mb-3 row">
-                                    <label for="inputMajor" class="col-sm-2 col-form-label">Major</label>
-                                    <div class="col-sm-10">
-                                        <select id="inputMajor" name="inputMajor" class="form-select" aria-label="Default select example" required>
-                                            <option selected value="">select</option>
-                                            <?php include "backend/getMajors.php" ?>
-                                        </select>
-                                    </div>
-                                </div>
-                                <script>
-                                    var DECIMAL_REGEXP = /(?<=^.{1}$)/g;
-
-                                    function handleKeyUp(e) {
-                                        var target = e.target;
-                                        var charCode = e.which || e.keyCode;
-
-                                        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-                                            return false;
-                                        }
-
-                                        target.value = target.value.replace(DECIMAL_REGEXP, '.');
-
-                                        return true;
-                                    }
-                                </script>
-                                
-                                <hr style="background-color: #e4e7ef; opacity: 1;">
-                                <div class="mb-3 row">
-                                    <label for="inputUsername" class="col-sm-2 col-form-label">Username</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" disabled class="form-control" id="inputUsername" name="inputUsername" required>
-                                    </div>
-                                </div>
-                                <div class="mb-3 row">
-                                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" disabled class="form-control" id="inputPassword" name="inputPassword" pattern="^(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{10,}$" title="Must contain at least one number and one special character, and at least 10 or more characters" required>
-                                    </div>
-                                </div>
-                                <script type="text/javascript">
-                                    inputInstructorName.addEventListener('keyup', function() {
-                                        if ($('#inputInstructorName').val()) {
-                                            document.getElementById('inputUsername').disabled = !this.value
-                                            document.getElementById('inputPassword').disabled = !this.value
-                                            var usernameFormat = "I" + document.getElementById('inputInstructorName').value.replaceAll(/\s/g,'');
-                                            var passwordFormat = "I#" + document.getElementById('inputInstructorName').value.replaceAll(/\s/g,'');
-                                        
-                                            $('#inputUsername').val(usernameFormat);
-                                            $('#inputPassword').val(passwordFormat);
-                                        } else {
-                                            document.getElementById('inputUsername').disabled = !this.value
-                                            document.getElementById('inputPassword').disabled = !this.value
-                                            $('#inputUsername').val("");
-                                            $('#inputPassword').val("");
-                                        };
-                                    });
-                                </script>
-                                <button type="submit" class="btn btn-primary float-end" id="registerManually" name="registerManually">Submit</button>
-                            </form>
-                        </div>
-                    </div>
-
-                    <div class="tab-pane fade" id="nav-csv" role="tabpanel" aria-labelledby="nav-csv-tab">
-                    <div class="card-body">
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="card">
+            <div class="card-body">
                 <p class="text mb-0">Remove Instructor</p>
                 <table class="table">
                     <thead>
                         <tr>
-                            <th scope="col">Name</th>
                             <th scope="col">Major</th>
+                            <th scope="col">Name</th>
                             <th scope="col">
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php include "backend/getInstructor.php"; ?>
+                        <?php include "backend/getInstructors.php"; ?>
                     </tbody>
                 </table>
             </div>
-        </div>
-        <!-- end -->
-                        
         </div>
         <!-- end -->
 
