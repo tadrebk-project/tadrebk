7d8c412684a668a33e7292b3f20632c175763b71
sasmaq <37103211+sasmaq@users.noreply.github.com>
2021-04-05 16:07:08 +0300

    A better final conn

--- general_backend/conn.php
+++ general_backend/conn.php
@@ -1,37 +1,33 @@
 <?php
-    $whitelist = array(
+
+$whitelist = array(
     '127.0.0.1',
     '::1'
-    );
+);
     
 if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
-            $host = "localhost";
-            $username = "root";
-            $password = "";
-            $db_name = "tadreabk";
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "tadreabk";
 } 
 else {
-            $host = getenv('MYSQLCONNSTR_HOST');
-            $username = getenv('MYSQLCONNSTR_USERNAME');
-            $password = getenv('MYSQLCONNSTR_PASSWORD');
-            $db_name = 'tadreabk';
+    $host = getenv('MYSQLCONNSTR_HOST');
+    $password = getenv('MYSQLCONNSTR_PASSWORD');
+    $username = getenv('MYSQLCONNSTR_USERNAME');
+    $db_name = 'tadreabk';
 }
 
 //Initializes MySQLi
 $conn = mysqli_init();
 
-
-
 // Establish the connection
 mysqli_real_connect($conn, $host, $username, $password , $db_name , 3306, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
-
-
-
 
 //If connection failed, show the error
 if (mysqli_connect_errno($conn))
 {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 ?>
