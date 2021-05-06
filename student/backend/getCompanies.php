d3154c230be459a6d3707aa362d91fa58a750f7a
Mohammad Muneer <mohammadesk42@gmail.com>
2021-05-06 20:59:11 +0300

    Update getCompanies.php

--- student/backend/getCompanies.php
+++ student/backend/getCompanies.php
@@ -36,66 +36,62 @@ $query = "SELECT DISTINCT c.* from company c LEFT JOIN application a on a.compID
 if($result=mysqli_query($conn, $query)){
     if(mysqli_num_rows($result)>0){
         $str = "";
+        $stra = "";
         while($row=mysqli_fetch_array($result)){
-                $str2 ="<div>
-                <h6>Majors: </h6>";
-                $query2 = "Select distinct m.name from requiredmajors r join major m on m.MID = r.MID join application a on a.appID = r.appID where a.compID = ".$row['compID'];
-                if($result2=mysqli_query($conn, $query2)){
-                    if(mysqli_num_rows($result2)>0){
-                        while($row2=mysqli_fetch_array($result2)){
-                            $str2 .= $row2["name"].", ";
+
+            $query = "SELECT * FROM application where compID=" . $row['compID'];
+            if ($resulta = mysqli_query($conn, $query)) {
+                if (mysqli_num_rows($resulta) > 0) {
+                    $stra = "";
+                    while ($rowa = mysqli_fetch_array($resulta)) {
+                        $trainingType = "";
+                        if ($rowa['trainingType'] == 'summer') {
+                            $trainingType = "Summer Training";
+                        } elseif ($rowa['trainingType'] == 'COOP') {
+                            $trainingType = "CO-OP";
                         }
-                        $str2 = substr($str2, 0, -2);
-                        $str2 .= "</div>";
-                    }
-                    else{
-                        $str2 ="";
-                    }
-                }
-                $str3 ="<div>
-                <h6>Training Type: </h6>";
-                $query3 = "Select distinct trainingType from application where compID = ".$row['compID'];
-                if($result2=mysqli_query($conn, $query3)){
-                    if(mysqli_num_rows($result2)>0){
-                        while($row3=mysqli_fetch_array($result2)){
-                            $trainingType = "";
-                            if($row3['trainingType']=='summer'){
-                                $trainingType = "Summer Training";
+                        $strm = "";
+                        $query2 = "SELECT name from major where MID in (SELECT MID from requiredmajors where appID = '" . $rowa['appID'] . "')";
+                        if ($resultm = mysqli_query($conn, $query2)) {
+                            if (mysqli_num_rows($resultm) > 0) {
+                                while ($rowm = mysqli_fetch_array($resultm)) {
+                                    $strm .= $rowm["name"] . ", ";
+                                }
+                                $strm = substr($strm, 0, -2);
+                            } else {
+                                $strm = "No majors specified...";
                             }
-                            elseif($row3['trainingType']=='COOP'){
-                                $trainingType = "CO-OP";
-                            }
-                            $str3 .= $trainingType."/";
                         }
-                        $str3 = substr($str3, 0, -1);
-                        $str3 .= "</div>";
+
+                    $stra .="<div class='collapse' id='collapse".$row['compID']."'>
+                                <div class='card ms-4 mb-1'>
+                                    <div class='card-body p-1'>
+                                        <div class='row row-cols-4'>
+                                            <div class='col'>
+                                                <p class='my-0'>" . $rowa['name'] . "</p>
+                                            </div>
+                                            <div class='col'>
+                                                <p class='my-0'>Training type: $trainingType</p>
+                                            </div>
+                                            <div class='col'>
+                                                <p class='my-0'>Majors: $strm</p>
+                                            </div>
+                                            <div class='col d-flex justify-content-end align-items-center'>
+                                                <a href='#' class='text-decoration-none'>
+                                                    <i class='d-flex align-items-center bi bi-arrow-right-square-fill me-3'
+                                                        style='font-size: 1.5em;'></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>";
                     }
-                    else{
-                        $str3 ="";
-                    }
+                } else {
+                    $stra = "";
                 }
-                
-            $str .= "<div class='card my-1'>
-            <div class='card-body'>
-                <div class='row row-cols-3'>
-                    <div class='col' data-bs-toggle='collapse' data-bs-target='#collapse".$row['compID']."'>
-                        <h5 class='my-0'>".$row['name']."</h5>
-                    </div>
-                    <div class='col' data-bs-toggle='collapse' data-bs-target='#collapse".$row['compID']."'>
-                        <p class='my-0'>".$row['location']."</p>
-                    </div>
-                    <div class='col'>
-                        <div class='container d-flex justify-content-center'>
-                            <a href='Company_desc.php?compID=".$row['compID']."' class='btn btn-primary py-0'>View</a>
-                        </div>
-                    </div>
-                </div>
-                <div class='collapse' id='collapse".$row['compID']."'>
-                    ".$str3.$str2."
-                </div>
-            </div>
-            </div>";
-/*
+            }
+
             $str .='
             <div class="card my-1">
                 <div class="card-body">
@@ -115,56 +111,11 @@ if($result=mysqli_query($conn, $query)){
                 </div>
             </div>
             <div class="d-flex flex-column">
-                <div class="collapse" id="collapse'.$row['compID'].'">
-                    <div class="card ms-4 mb-1">
-                        <div class="card-body p-1">
-                            <div class="row row-cols-4">
-                                <div class="col">
-                                    <p class="my-0">Opportunity name</p>
-                                </div>
-                                <div class="col">
-                                    <p class="my-0">Training type: CO-OP</p>
-                                </div>
-                                <div class="col">
-                                    <p class="my-0">Majors: </p>
-                                </div>
-                                <div class="col d-flex justify-content-end align-items-center">
-                                    <a href="#" class="text-decoration-none">
-                                        <i class="d-flex align-items-center bi bi-arrow-right-square-fill me-3"
-                                            style="font-size: 1.5em;"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="collapse" id="collapse'.$row['compID'].'">
-                    <div class="card ms-4 mb-1">
-                        <div class="card-body p-1">
-                            <div class="row row-cols-4">
-                                <div class="col">
-                                    <p class="my-0">Opportunity name</p>
-                                </div>
-                                <div class="col">
-                                    <p class="my-0">Training type: '.'</p>
-                                </div>
-                                <div class="col">
-                                    <p class="my-0">Majors: '.'</p>
-                                </div>
-                                <div class="col d-flex justify-content-end align-items-center">
-                                    <a href="#" class="text-decoration-none">
-                                        <i class="d-flex align-items-center bi bi-arrow-right-square-fill me-3"
-                                            style="font-size: 1.5em;"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                '.$stra.'
             </div>
             <!----> 
             ';
-*/
+
         }
         
         echo $str;
