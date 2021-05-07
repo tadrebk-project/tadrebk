8df8dc4d5f1ef21978ae927b03da295d8083bc46
sasmaq <37103211+sasmaq@users.noreply.github.com>
2021-05-07 22:57:22 +0300

    Update adminHome.php

--- admin/adminHome.php
+++ admin/adminHome.php
@@ -23,6 +23,7 @@ if (file_exists("../general_backend/sessionStart.php")) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <script src="https://kit.fontawesome.com/320bfa98c5.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="admin.css">
 
     <link rel="icon" type="image/png" href="../general_resources/Tadreabk_favicon.png"/>
