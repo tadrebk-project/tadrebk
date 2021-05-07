2ff7a979bdc7e5ce83ee11063bfcc34e846a5944
Mohammad Owaidah <eng.mmo7@gmail.com>
2021-05-08 01:35:23 +0300

    mmm

--- student/backend/updateProfile.php
+++ student/backend/updateProfile.php
@@ -26,7 +26,9 @@ if (isset($_POST['saveButton'])) {
     if ($checkemail) {
         mysqli_close($conn);
         //$error= "Email is already used by another user!";
+        echo "kkk";
         echo "<script>alert('Email is already used by another user!'); window.location.href='../profile.php';</script>";
+        echo"teset";
 
     }
     else{
@@ -43,5 +45,5 @@ if (isset($_POST['saveButton'])) {
         mysqli_close($conn);
     }
 }
-header('location: ../profile.php');
+//header('location: ../profile.php');
 ?>
